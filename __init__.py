40d96e3963ec65e5114df4675b579ef5df028957
Riccardo Campana <riccardo.campana@inaf.it>
2014-10-22 16:25:28 +0200

    Initial commit

---
+++ __init__.py
@@ -0,0 +1,21 @@
+"""
+A python package to simulate X-ray lightcurves
+from coherent signals and power spectrum models.
+"""
+
+__author__ = 'Riccardo Campana'
+__version__ = '0.1'
+
+from simulation import Simulation
+from lcsinusoid import lcsinusoid
+from lcpsd import lcpsd
+from utils import poisson_randomization
+from utils import psd
+from utils import rebin
+from utils import logrebin
+from utils import saveFITSLC
+from utils import saveFITSPSD
+
+__all__ = ['Simulation','lcsinusoid', 'lcpsd', 'poisson_randomization', 'psd', 'rebin', 'logrebin', 'saveFITSLC', 'saveFITSPSD']
+
+
---
+++ psd_models/__init__.py
@@ -0,0 +1,9 @@
+"""
+Contains the analytic models for the power spectrum density.
+"""
+
+
+__all__ = ["lorentzian", "smoothbknpo"]
+
+from lorentzian import lorentzian
+from smoothbknpo import smoothbknpo
