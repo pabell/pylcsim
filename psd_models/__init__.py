e1e25e1728582d9577d5655fae15a78cc574aebb
Riccardo Campana <riccardo.campana@inaf.it>
2014-10-22 17:32:00 +0200

    Next try...

--- psd_models/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Contains the analytic models for the power spectrum density.
-"""
-
-
-__all__ = ["lorentzian", "smoothbknpo"]
-
-from lorentzian import lorentzian
-from smoothbknpo import smoothbknpo
